024f1e6a3d0d62807524e88d9029acf770d34de2
TMAdevby <mtipun@mail.ru>
2025-11-12 14:50:14 +0300

    random iter

---
+++ src/main/java/ru/netology/Randoms.java
@@ -0,0 +1,35 @@
+package ru.netology;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    private final int min;
+    private final int max;
+    private final Random random;
+
+    public Randoms(int min, int max) {
+        if (min > max) {
+            throw new IllegalArgumentException("Минимальное значение не может быть больше максимального");
+        }
+        this.min = min;
+        this.max = max;
+        this.random = new Random();
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt(max - min + 1) + min;
+            }
+        };
+    }
+}
